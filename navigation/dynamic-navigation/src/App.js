92d6ee019b35f0d430bae2297c27468736f444ce
Bill <ceriustech@gmail.com>
2021-11-24 21:45:51 -0600

    added new directory for vertical navigation

--- navigation/dynamic-navigation/src/App.js
+++ navigation/dynamic-navigation/src/App.js
@@ -1,27 +1,21 @@
-import React from "react";
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
-import { navItems } from "./data";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { navItems } from './data';
 import './Styles/GenericPage.Styles.scss';
 
-
-import NavigationContainer from "./components/NavigationContainer";
+import NavigationContainer from './components/NavigationContainer';
 
 const App = () => {
-  const pageComponentArray = navItems.map(({path, component}, key) => <Route exact path={path} component={component} key={key} />);
+	const pageComponentArray = navItems.map(({ path, component }, key) => (
+		<Route exact path={path} component={component} key={key} />
+	));
 
-  return (
-    <div className="App">
-      <NavigationContainer />
-          <Switch>
-           {pageComponentArray}
-          </Switch>
-    
-      
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<NavigationContainer />
+			<Switch>{pageComponentArray}</Switch>
+		</div>
+	);
+};
 
 export default App;
